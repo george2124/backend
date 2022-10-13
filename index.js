3fdd4d3085dc70e47f299115687fb8cb5aca7a17
Jorge Apaza <jorgeapaza2124@gmail.com>
2022-10-13 17:44:02 -0300

    desafio de class

---
+++ index.js
@@ -0,0 +1,56 @@
+class usurio {
+    constructor( nombre, apellido ) {
+         this.nombre = nombre;
+         this.apellido = apellido;
+         this.libros = [ ];
+         this.mascotas = [ ];   
+    }
+
+//Metodo que devuelve el nombre completo del usuario
+getFullName(){
+    return `${this.nombre} ${this.apellido}`;
+}    
+
+//Metodo que agrega mascotas
+addMascotas(){
+    this.mascotas = [...this.mascotas, mascotas ];
+}
+
+//Metodo que devuelve la cantidad de mascotas del usuario
+countMascotas(){
+    return this.mascotas.length;
+}
+
+//Metodo para agregar libros, que recibe el titulo y el autor del libro
+addBook( titulo, autor ){
+    this.libros = [...this.libros, {titulo, autor}];
+}
+
+//Metodo que me devuelve los nombres de libros del usuario
+getBookName(){
+    const booknames = []
+    this.libros.map( item => (
+        booknames = [...booknames, item.titulo]
+        )
+    )
+    return booknames;
+}
+}
+
+
+const usuario = new usuario("Richards", "Duglas");
+
+
+usuario.addMascotas("perro");
+usuario.addMascotas("gato");
+
+
+usuario.libros("CANTOS DE VIDA Y ESPERANZA Y OTROS POEMAS", "RUBEN DARIO");
+usuario.libros("EL CLUB DE LA 5 DE LA MAÑANA", "ROBIN SHARMA");
+usuario.libros("RESULTADOS EXTRAORDINARIOS", "BERNARDO STAMATEAS");
+usuario.libros("TODO EL AMOR", "PABLO NERUDA");
+
+
+console.log("Usurio:" + usuario.getFullName)
+console.log('Cantidad de mascotas'+ usuario.countMascotas)
+console.log('Cantidad de libros'+ usuario.getBookName)
